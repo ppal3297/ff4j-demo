472de922c61cf915dc37e72387f07c98f5a05d46
Pankaj <pal.pankaj3297@gmail.com>
2022-01-23 16:59:02 +0530

    empty project added

---
+++ src/main/java/com/pankaj/ff4jdemo/Ff4jDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pankaj.ff4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ff4jDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ff4jDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pankaj/ff4jdemo/Ff4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankaj.ff4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ff4jDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
